1cda780ea49803f9282299a387418ece174f7208
Francesco <fran131b@easv365.dk>
2020-10-14 08:33:11 +0200

    Changed to ClassInitalize

--- TestPerformance/PerformanceTest.cs
+++ TestPerformance/PerformanceTest.cs
@@ -7,79 +7,84 @@ namespace TestPerformance
     public class PerformanceTest
     {
         private static IRatingRepository rr;
+        [ClassInitialize]
+        public static void SetupRepo(TestContext tc)
+        {
+            rr = new RatingRepository();
+        }
 
         [TestMethod]
-        [Timeout(20000)]
+        [Timeout(4000)]
         public void TestGetAverageRateFromReviewer()
         {
             rr.GetAverageRateFromReviewer(1);
         }
 
         [TestMethod]
-        [Timeout(20000)]
+        [Timeout(4000)]
         public void TestGetAverageRateOfMovie()
         {
             rr.GetAverageRateOfMovie(558634);
         }
 
         [TestMethod]
-        [Timeout(25000)]
+        [Timeout(4000)]
         public void TestGetMostProductiveReviewers()
         {
             rr.GetMostProductiveReviewers();
         }
 
         [TestMethod]
-        [Timeout(20000)]
+        [Timeout(4000)]
         public void TestGetMoviesWithHighestNumberOfTopRates()
         {
             rr.GetMoviesWithHighestNumberOfTopRates();
         }
 
         [TestMethod]
-        [Timeout(20000)]
+        [Timeout(4000)]
         public void TestGetNumberOfRates()
         {
             rr.GetNumberOfRates(662870, 5);
         }
 
         [TestMethod]
-        [Timeout(20000)]
+        [Timeout(4000)]
         public void TestGetNumberOfRatesByReviewer()
         {
             rr.GetNumberOfRatesByReviewer(331, 4);
         }
 
         [TestMethod]
-        [Timeout(20000)]
+        [Timeout(4000)]
         public void TestGetNumberOfReviews()
         {
             rr.GetNumberOfReviews(90668);
         }
 
         [TestMethod]
-        [Timeout(20000)]
+        [Timeout(4000)]
         public void TestGetNumberOfReviewsFromReviewer()
         {
             rr.GetNumberOfReviewsFromReviewer(199);
         }
 
         [TestMethod]
-        [Timeout(20000)]
+        [Timeout(4000)]
         public void TestGetReviewersByMovie()
         {
             rr.GetReviewersByMovie(1890591);
         }
 
         [TestMethod]
-        [Timeout(20000)]
+        [Timeout(4000)]
         public void TestGetTopMoviesByReviewer()
         {
             rr.GetTopMoviesByReviewer(12);
         }
 
         [TestMethod]
-        [Timeout(20000)]
+        [Timeout(4000)]
         public void TestGetTopRatedMovies()
         {
             rr.GetTopRatedMovies(3);
